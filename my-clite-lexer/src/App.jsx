e4bed28a4c3aafe7d167881bcdd1b3f42a5a1e27
Jessica De-Paz <T0141000@miamidade.gov>
2025-03-08 23:23:57 -0500

    Basic Frontend with JavaScript Lexer

---
+++ my-clite-lexer/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';  // Make sure to include the CSS file for styling
+import Lexer from './Lexer';  // Import Lexer component
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Group 1: Lexical Analyzer Assignment 3</h1>
+      {/* Render the Lexer component */}
+      <Lexer />
+    </div>
+  );
+};
+
+export default App;
